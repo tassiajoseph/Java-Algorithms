092b9fe37fec3c57f54c723b4c8e5973d4280c5f
Tassia Joseph <tassiajoseph.student@careerdevs.com>
2021-12-16 11:54:49 -0500

    adding getfirstelement algorithm

---
+++ src/GetFirstElement.java
@@ -0,0 +1,12 @@
+public class GetFirstElement {
+
+    public static void main(String[] args) {
+
+        System.out.println( getFirstValue( new int[] {1, 2, 3, 4, 5} ) );
+    }
+
+    public static int getFirstValue(int[] arr) {
+        // arrays: type[] varName;
+        return arr[0];
+    }
+}
