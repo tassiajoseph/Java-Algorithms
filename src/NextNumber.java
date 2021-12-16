9c7c723ec57827abdd6a855beda79fca1bf77b7a
Tassia Joseph <tassiajoseph.student@careerdevs.com>
2021-12-16 13:48:35 -0500

    updating repo

--- src/NextNumber.java
+++ src/NextNumber.java
@@ -7,8 +7,7 @@ public class NextNumber {
     /* ===================================================
         Create a function that takes a number as an argument,
         increments the number by +1 and returns the result.
-        ---update
-    ====================================================*/
+    ====================================================== */
 
     public static int addition(int num) {
         return num+1;
